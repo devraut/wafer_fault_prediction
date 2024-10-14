2078fef34b4ed561de0dba8169390d3f569a4846
devraut <devraut806@gmail.com>
2024-10-14 22:24:37 +0545

    resolving issues

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="wafer-fault-detection",
-    version="0.0.3",
+    version="0.0.1",
     description="ML project",
-    author="Avnish yadav", 
+    author="Dev Ranjan Raut",
     packages=find_packages(),
     license="MIT"
 )
